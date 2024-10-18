ebe1bfd657ed61f50cba110685dcdc9d3f1b7e62
scrsund <137787953+scrsund@users.noreply.github.com>
2024-10-18 12:49:30 +0200

    adding router

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/tailwind.css";
 import store from './store/index.js';
+import router from './router.js';
 
 const app = createApp(App);
 
 app.use(store);
+app.use(router);
+
 app.mount("#app");
