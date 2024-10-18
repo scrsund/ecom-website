e4292788263546b61c559d9b1b36760731d715ae
scrsund <137787953+scrsund@users.noreply.github.com>
2024-10-18 23:02:38 +0200

    centering header

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,17 +1,19 @@
 <template>
-  <header class="fixed top-0 left-0 right-0 z-50 flex justify-between items-center py-8 px-5 bg-white border-b shadow-sm md:p-10">
-    <div class="header-items">
-      <button @click="toggleMenu" class="md:uppercase">
-        <span @mouseenter="showMenu" class="hidden md:block">Shop</span>
-        <HamburgerButton></HamburgerButton>
-      </button>
-      <span class="hidden md:block">|</span>
-      <a href="#" class="hidden md:block">Search</a>
-    </div>
-    <router-link to="/home" class="text-3xl tracking-widest ml-7 md:ml-0">Othelia</router-link>
-    <div class="header-items">
-      <a href="#" class="hidden md:block">Account</a>
-      <a href="#">Bag <span class="tracking-widest">(0)</span></a>
+  <header class="fixed top-0 left-0 right-0 z-50 py-8 px-5 bg-white border-b shadow-sm md:p-10">
+    <div class="flex justify-between items-center">
+      <div class="header-items justify-start">
+        <button @click="toggleMenu" class="md:uppercase">
+          <span @mouseenter="showMenu" class="hidden md:block">Shop</span>
+          <HamburgerButton></HamburgerButton>
+        </button>
+        <span class="hidden md:block">|</span>
+        <a href="#" class="hidden md:block">Search</a>
+      </div>
+      <router-link to="/home" class="text-3xl tracking-widest">Othelia</router-link>
+      <div class="header-items justify-end">
+        <a href="#" class="hidden md:block">Account</a>
+        <a href="#">Bag <span class="tracking-widest">(0)</span></a>
+      </div>
     </div>
   </header>
 </template>
