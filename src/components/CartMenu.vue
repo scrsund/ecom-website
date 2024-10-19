d7b4a27a9f8ea57919f6a51f062e7e735abbd8e1
scrsund <137787953+scrsund@users.noreply.github.com>
2024-10-19 15:13:58 +0200

    checking if the bag is empty

--- src/components/CartMenu.vue
+++ src/components/CartMenu.vue
@@ -1,27 +1,32 @@
 <template>
-  <!-- Page Container -->
-  <div v-show="isOpen" class="fixed inset-0 z-40 h-full w-full">
-    <div class="bg-zinc-100 z-50 h-full w-2/3 shadow-xl">
-      <button @click="closeCart" class="z-100">Close</button>
-
+  <!-- Page Outline -->
+  <div v-show="isOpen" class="fixed top-0 right-0 z-40 h-full">
+    <!-- Menu Container -->
+    <div class="z-50 flex flex-col h-full w-[350px] bg-white shadow-xl">
+      <!-- Close button -->
+      <button @click="closeCart" class="z-100 text-right p-4">Close</button>
+      <!-- Content Container -->
+      <div>
+        <h1 v-if="this.$store.getters.cart === 0" class="text-center">Your bag is empty</h1>
+        <h1 v-else>You have {{this.$store.getters.cart}} items in your bag</h1>
+      </div>
     </div>
   </div>
 </template>
 
-
 <script>
-import { mapState, mapMutations } from 'vuex';
+import { mapState, mapMutations } from "vuex";
 
 export default {
   computed: {
-    ...mapState({isOpen: state => state.cart.isOpen}),
+    ...mapState({ isOpen: (state) => state.cart.isOpen }),
   },
   methods: {
     ...mapMutations({
-      closeCart: 'closeCart'
+      closeCart: "closeCart",
     }),
-  }
-}
+  },
+};
 </script>
 <!-- 
 
